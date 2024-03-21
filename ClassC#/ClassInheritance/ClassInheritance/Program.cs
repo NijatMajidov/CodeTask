ab440e541220d6c8c8d2bda478f45d0ae2ee5f72
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-03-22 00:50:12 +0400

    Entry to class

---
+++ ClassC#/ClassInheritance/ClassInheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ClassInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+    }
+
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string DepartmentName;
+        public double Salary;
+
+        public Employee(string name, string surname, byte age, string departmentName, double salary)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            DepartmentName = departmentName;
+            Salary = salary;
+        }
+    }
+
+    class Department
+    {
+        Employee[] employees = { };
+        public AddEmploye()
+        {
+
+        }
+    }
+
+}
