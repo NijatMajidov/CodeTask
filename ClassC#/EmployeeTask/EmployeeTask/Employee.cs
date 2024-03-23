c83aec5d913d0035c10bfb34220c5076c1514ea1
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-03-23 21:45:31 +0400

    Employee class was created

---
+++ ClassC#/EmployeeTask/EmployeeTask/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    internal class Employee
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string DepartmentName;
+        public double Salary;
+
+        public Employee(string name, string surname, byte age, string departmentName, double salary)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            DepartmentName = departmentName;
+            Salary = salary;
+        }
+        #region ShowEmployeeInfo
+        public void ShowEmployeeInfo()
+        {
+            Console.WriteLine(GetEmployeeInfo());
+        }
+
+        public string GetEmployeeInfo()
+        {
+            return $"{this.Name} {this.Surname}, |Age:{this.Age}|, Department name |{this.DepartmentName}|, |Salary:{this.Salary}|";
+        }
+        #endregion
+
+    }
+}
---
+++ ClassC#/EmployeeTask/EmployeeTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassTask;
+
+namespace EmployeeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee user = new Employee("Nicat", "Mecidov", 19, "BB207", 1024);
+            Employee user1 = new Employee("Resid", "Babazade", 37, "BB207", 512);
+            Employee user2 = new Employee("Asime", "Quliyeva", 24, "BB207", 256);
+
+
+            
+        }
+    }
+}
